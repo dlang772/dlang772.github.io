79cb825d9077bcf9d7528c7a58e23ab884eb0a57
Dan Lang <danlang@Dans-MacBook-Pro.local>
2017-06-18 16:49:11 -0400

    update signature

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function() {
 	$('a[href*=#]').on('click', function(e) {
 		e.preventDefault();
-		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 50}, 500, 'linear');
 	});
 });
 
