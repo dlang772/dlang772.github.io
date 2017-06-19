c7ca0e77ebce82e3efd5a7445ff3b8cba0c5afa3
Dan Lang <danlang@Dans-MBP.T-mobile.com>
2017-06-19 03:52:16 -0400

    added new portfolio page and photography website link

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ $(function() {
         	scrollTop: ($("#thanks").offset().top - 50)}, 500);
         return false;
     });
-     $("#thanks").click(function() {
+    $("#thanks").click(function() {
         $('html, body').animate({ 
         	scrollTop: ($("#section10").offset().top - 50)}, 500);
         return false;
     });
 
+    // Function to make "About" scroll down if eventually I change the layout
+    // ##############################
+    //  $("#navbar-about").click(function() {
+    //     $('html, body').animate({ 
+    //     	scrollTop: ($("#thanks").offset().top - 50)}, 500);
+    //     return false;
+    // });
 });
