3a18c041efce0acfb53382b438e0e73aff291499
Dan Lang <danlang@Dans-MBP.T-mobile.com>
2017-06-18 23:17:38 -0400

    domain name

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(function() {    
     $("#section10").click(function() {
         $('html, body').animate({ 
-        	scrollTop: ($("#thank-you").offset().top - 50)}, 500);
+        	scrollTop: ($("#thanks").offset().top - 50)}, 500);
         return false;
     });
-     $("#thank-you").click(function() {
+     $("#thanks").click(function() {
         $('html, body').animate({ 
         	scrollTop: ($("#section10").offset().top - 50)}, 500);
         return false;
